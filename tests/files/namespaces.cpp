370ddb2c3955ba8dac3eb536138f9950c78c7f35
Milian Wolff <mail@milianw.de>
2015-10-08 15:49:52 +0200

    kdev-clang is now merged into kdevelop.

--- tests/files/namespaces.cpp
+++
@@ -1,9 +0,0 @@
-namespace bar { namespace asdf {} };
-
-/// "toString" : "Import bar as blub",
-/// "kind" : "NamespaceAlias"
-namespace blub = bar;
-
-/// "toString" : "Import of blub::asdf",
-/// "kind" : "NamespaceAlias"
-using namespace blub::asdf;
--- tests/files/special.cpp
+++
@@ -1,8 +0,0 @@
-/// "isDeprecated" : "true"
-__attribute__ ((deprecated)) int deprecatedfunc() {}
-/// "isDeprecated" : "false"
-int nodeprecatedfunc() {}
-/// "isDeprecated" : "true"
-__attribute__ ((deprecated)) static const int deprecatedvar;
-/// "isDeprecated" : "false"
-static const int nodeprecatedvar;
